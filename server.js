08db05a4a9271ecaaf2b020ff6276aa2279129c4
zacharyhaider <42784940+zacharyhaider@users.noreply.github.com>
2019-11-20 13:40:17 -0900

    Removed unused files.

--- server.js
+++
@@ -1,6 +0,0 @@
-var express=require('express');
-var app=express();
-app.get('/',(function(req,res){
-	res.sendFile('C:\\Users\\s516936\\Desktop\\UXD\\UXD_FP_S1_T3\\templates\\index.html');
-}));
-app.listen(8080, () => console.log(`App listening on port 8080!`))
